c672c7b7882cadaefc0498eabfa1ac03a6787a1a
Johan Mejia <112667439+JohanMejiaM@users.noreply.github.com>
2023-03-04 09:56:56 -0500

    Add files via upload

---
+++ BackendVet-main/projectVet/index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import { usuarioRouter } from "./router/usuarios.router.js";
+import { citasRouter } from "./router/citas.router.js";
+import { petRouter } from "./router/pet.router.js";
+import cors from 'cors';
+import mongoose from "mongoose";
+import { validarToken } from "./utils/validador.js";
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+server.options('*', cors()); // include before other routes
+
+
+server.all("/citas",validarToken)
+server.all("/citas/:id",validarToken)
+server.all("/usuarios",validarToken)
+server.all("/usuarios/:id",validarToken)
+server.all("/pet",validarToken)
+server.all("/pet/:id",validarToken)
+
+server.use(usuarioRouter);
+server.use(citasRouter);
+server.use(petRouter);  
+
+
+const puerto = process.env.PORT ?? 3000;
+
+
+server.listen(3000,()=>{
+    console.log(`Servidor corriendo correctamente en el puerto ${puerto}`)
+    mongoose.set("strictQuery",true);
+    mongoose.connect(process.env.MONGO_URL).then((valor)=>{
+        console.log("Se conecto exitosamente a la base de datos")
+    }).catch((error)=>{
+        console.log("Error al conectarse")
+    })
+})
